18c0e8146ff059ecc8ff4e9192b9bc62390105dc
SergioF2203 <fs220381@gmail.com>
2019-04-10 20:48:38 +0300

    created form from add-item implemented to add new Item to List

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -4,14 +4,39 @@ import './add-item.css';
 
 export default class AddItem extends Component {
 
+    state = {
+        label: ''
+    };
+
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+       this.props.addNewItem(this.state.label);
+    };
+
     render() {
         return (
-            <div className='add-item'>
-                <button className='btn btn-outline-secondary'
-                onClick={()=>this.props.addNewItem('new item ...')}>
-                    Add Item
+            <form className='add-item d-flex'
+                  onSubmit={this.onSubmit}>
+                <input
+                    type='text'
+                    className="form-control"
+                    onChange={this.onLabelChange}
+                    placeholder='what needs to be done'
+                />
+                <button
+                    className='btn btn-outline-secondary'
+                    // onClick={() => this.props.addNewItem('new item ...')}
+                >
+                    ...
                 </button>
-            </div>
+            </form>
         )
     }
 };
